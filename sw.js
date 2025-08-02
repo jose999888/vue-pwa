d7a154c5883b6920e7e53bbe5f15e34a14814303
jose999888 <jose999888@users.noreply.github.com>
2025-08-02 08:31:54 +0000

    deploy: b21312dc76447500158c58ba42c993a334280dbb

---
+++ sw.js
@@ -0,0 +1,37 @@
+const version = 'v6'
+const CURRENT_CACHE = `vue-pwa-${version}`
+const urlToCache = []
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CURRENT_CACHE).then((cache) => {
+      return cache.addAll(urlToCache)
+    }),
+  )
+  self.skipWaiting()
+})
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys()
+      await Promise.all(
+        keys.map((key) => {
+          if (key !== CURRENT_CACHE) {
+            return caches.delete(key)
+          }
+          return Promise.resolve()
+        }),
+      )
+    })(),
+  )
+  self.clients.claim()
+  console.log('Service worker activated')
+})
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((r) => {
+      if (r) console.log('命中缓存', r)
+      return r || fetch(event.request)
+    }),
+  )
+  console.log(`URL requested: ${event.request}`)
+})
