ccb8617781925a88c29844d118e951cbc33844d4
jose999888 <168402067+jose999888@users.noreply.github.com>
2025-08-03 15:37:59 +0700

    33

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -2,11 +2,15 @@
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
+function reload() {
+  location.reload()
+}
 </script>
 <template>
   <div>
     <h1>这个是AppHome页面</h1>
     <h2>自动更新</h2>
     <button @click="router.push('/about')">切换页面</button>
+    <button @click="reload">刷新页面</button>
   </div>
 </template>
