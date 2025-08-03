eb68945467330cefa5deaafb5907561291d030c3
jose999888 <168402067+jose999888@users.noreply.github.com>
2025-08-03 15:51:13 +0700

    1

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -9,7 +9,7 @@ function reload() {
 <template>
   <div>
     <h1>这个是AppHome页面</h1>
-    <h2>自动更新要加reload</h2>
+    <h2>自动更新要加reload.啦啦啦啦啦啦啦</h2>
     <button @click="router.push('/about')">切换页面</button>
     <button @click="reload">刷新页面</button>
   </div>
