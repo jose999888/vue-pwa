a47f78704e9d194fb8344f9dce59682f375e65c5
jose999888 <168402067+jose999888@users.noreply.github.com>
2025-08-03 15:34:00 +0700

    22222

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -6,7 +6,7 @@ const router = useRouter()
 <template>
   <div>
     <h1>这个是AppHome页面</h1>
-    <h2>这个是用来测试是否可以自动更新的</h2>
+    <h2>自动更新</h2>
     <button @click="router.push('/about')">切换页面</button>
   </div>
 </template>
