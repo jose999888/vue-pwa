a2981e0916ae3940741ef1472220ea95f8a7a1ad
jose999888 <168402067+jose999888@users.noreply.github.com>
2025-08-03 16:36:41 +0700

    1

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -15,7 +15,6 @@ function goToTarget() {
   <div>
     <h1>这个是AppHome页面</h1>
     <h2>自动更新要加reload.啦啦啦啦啦啦啦</h2>
-    <h3>这次我改了js</h3>
     <button @click="goToTarget">切换页面</button>
     <button @click="reload">刷新页面</button>
   </div>
