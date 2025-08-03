8bf68cf565ef80c122508f496307fdb47b0dd622
jose999888 <168402067+jose999888@users.noreply.github.com>
2025-08-03 15:55:15 +0700

    1111

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -5,12 +5,18 @@ const router = useRouter()
 function reload() {
   location.reload()
 }
+function goToTarget() {
+  console.log('about')
+  alert('about')
+  router.push('/about')
+}
 </script>
 <template>
   <div>
     <h1>这个是AppHome页面</h1>
     <h2>自动更新要加reload.啦啦啦啦啦啦啦</h2>
-    <button @click="router.push('/about')">切换页面</button>
+    <h3>这次我改了js</h3>
+    <button @click="goToTarget">切换页面</button>
     <button @click="reload">刷新页面</button>
   </div>
 </template>
